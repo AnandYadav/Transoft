7b43a1f814ef2403fefd62df422d9f514a31ed33
Anand Yadav <aanand.18jan@gmail.com>
2016-08-28 20:15:47 +0530

    Initial commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Transoft' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+    pod 'CryptoSwift'
+
+
+# Pods for Transoft
+
+  target 'TransoftTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TransoftUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Transoft/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Transoft
+//
+//  Created by Anand Yadav on 06/08/16.
+//  Copyright © 2016 Transoft. All rights reserved.
+//
+
+import UIKit
+import CryptoSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
